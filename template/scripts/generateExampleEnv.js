610b955b7e0b14d83022e576625305f40f90dbf7
Matej <hozlarmatej0@gmail.com>
2025-07-24 16:30:31 +0200

    revamp

--- template/scripts/generateExampleEnv.js
+++
@@ -1,5 +0,0 @@
-import path from "path";
-import { generateEnvExampleFile } from "./src/generateExampleEnvFile.js";
-
-const outputPath = path.resolve(".env.example");
-generateEnvExampleFile(outputPath);
